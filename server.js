cb9c7715e16cf82c4c5fea3d463ec089ffb8a387
Joost Jansen <hello@joostjansen.me>
2021-07-19 22:51:14 +0200

    login, log out and redirect functionality done

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+// LIBRARIES
+
 const express = require('express')
 const bcrypt = require('bcrypt')
-
-const users = []
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
 
 // APP CONFIG
 
@@ -10,28 +18,50 @@ const app = express()
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 
+const initializePassport = require('./passport.config')
+initializePassport(passport, getUserByEmail, getUserById)
+
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+// DATA
+
+const users = []
+
 // HOME
 
-app.get('/', (req, res) => {
-  res.render('index.ejs', { name: 'there!' })
+app.get('/', guardAuthenticated, (req, res) => {
+  res.render('index.ejs', { name: req.user.name })
 })
 
 // LOGIN
 
-app.get('/login', (req, res) => {
+app.get('/login', guardNotAuthenticated, (req, res) => {
   res.render('login.ejs')
 })
 
-app.post('/login', (req, res) => {
-})
+const loginStrategy = {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}
+
+app.post('/login', guardNotAuthenticated, passport.authenticate('local', loginStrategy), (req, res) => { })
 
 // REGISTER
 
-app.get('/register', (req, res) => {
+app.get('/register', guardNotAuthenticated, (req, res) => {
   res.render('register.ejs')
 })
 
-app.post('/register', async (req, res) => {
+app.post('/register', guardNotAuthenticated, async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10)
     users.push({
@@ -47,6 +77,35 @@ app.post('/register', async (req, res) => {
   console.log(users)
 })
 
+app.delete('/logout', (req, res) => {
+  req.logOut()
+  res.redirect('/login')
+})
+
 // RUN APP
 
 app.listen(3000, () => console.log('Listening on port 3000...'))
+
+// FUNCTIONS
+
+function getUserByEmail(email) {
+  return users.find(u => u.email === email)
+}
+
+function getUserById(id) {
+  return users.find(u => u.id === id)
+}
+
+function guardAuthenticated(req, res, next) {
+  if (req.isAuthenticated())
+    return next()
+
+  return res.redirect('/login')
+}
+
+function guardNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated())
+    return res.redirect('/')
+
+  return next()
+}
